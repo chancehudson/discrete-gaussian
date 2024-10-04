54bf792bd61fb44a04983f2fdefdc96260524086
vimwitch <jchancehud@gmail.com>
2024-10-03 22:52:30 -0700

    fix: THETA_0 details

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ mod ctime;
 pub use vtime::sample_vartime;
 pub use ctime::sample;
 
-/// Approximation of sqrt(1/(2*ln(2))) to 20 decimal places
-/// |THETA_0 - sqrt(1/(2*ln(2)))| <= 2^-66
+/// Approximation of sqrt(1/(2*ln(2))) as defined
+/// in https://eprint.iacr.org/2018/1234.pdf page 6
 /// 
-/// as defined in https://eprint.iacr.org/2018/1234.pdf page 6
+/// Error due to IEEE754 conversion: -0.000000017699060302485625
+/// Actual stored value: 0.849321782588958740234375
 const THETA_0: f64 = 0.84932180028801904272;
