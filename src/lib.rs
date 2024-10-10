d49c251b1b98122b1d58e9c98392b3deb3596338
vimwitch <jchancehud@gmail.com>
2024-10-09 17:57:11 -0700

    chore: custom doc header

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
-#![doc = include_str!("../README.md")]
-#![doc(html_root_url = "https://docs.rs/discrete-gaussian")]
+//! [![Build](https://img.shields.io/circleci/build/github/chancehudson/discrete-gaussian/main)](https://dl.circleci.com/status-badge/redirect/gh/chancehudson/discrete-gaussian/tree/main) [![Docs](https://img.shields.io/docsrs/discrete-gaussian)](https://docs.rs/discrete-gaussian) [![Version](https://img.shields.io/crates/v/discrete-gaussian)](https://crates.io/crates/discrete-gaussian)
+//!
+//! Discrete gaussian samples based on [FACCT](https://eprint.iacr.org/2018/1234.pdf) and [DDLL13](https://eprint.iacr.org/2013/383.pdf).
+//! Implemented with ~40 bits of accuracy (relative error <= 2^-40).
+//! Based on the analysis in [WALT19](https://eprint.iacr.org/2019/068.pdf) this implies an upper bound of 2^80 security in constructions.
+//!
 
 pub mod ctime;
 pub mod vtime;
